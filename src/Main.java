49d6d746a3c61a9ae0046531d2e850c3fdfabb8f
klozz <ya.slava2323yaru@gmail.com>
2022-12-11 20:23:15 +0300

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+
+    public static void task1(){
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName+" "+firstName+" "+middleName;
+        System.out.println("ФИО сотрудника — "+fullName);
+        task2(fullName);
+        task3(fullName);
+    }
+
+    public static void task2(String fullName){
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+fullName.toUpperCase());
+    }
+
+    public static void task3(String fullName){
+        String changedName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + changedName);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
     }
 }
